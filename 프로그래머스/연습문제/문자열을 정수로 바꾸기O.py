ff78961f6b263b2f3138ddd3c484c1f2bc0b68cd
eastcold <67042697+kdch9495@users.noreply.github.com>
2021-11-05 15:09:50 +0900

    Update 문자열을 정수로 바꾸기O.py

--- 프로그래머스/연습문제/문자열을 정수로 바꾸기O.py
+++ 프로그래머스/연습문제/문자열을 정수로 바꾸기O.py
@@ -4,3 +4,21 @@
 def solution(s):
     return int(s)
 
+"""
+# 문제가 개편되었습니다. 이로 인해 함수 구성이나 테스트케이스가 변경되어, 과거의 코드는 동작하지 않을 수 있습니다.
+# 새로운 함수 구성을 적용하려면 [코드 초기화] 버튼을 누르세요. 단, [코드 초기화] 버튼을 누르면 작성 중인 코드는 사라집니다.
+def strToInt(str):
+    result = 0
+
+    for idx, number in enumerate(str[::-1]):
+        if number == '-':
+            result *= -1
+        else:
+            result += int(number) * (10 ** idx)
+
+    return result
+
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print(strToInt("-1234"));
+"""
