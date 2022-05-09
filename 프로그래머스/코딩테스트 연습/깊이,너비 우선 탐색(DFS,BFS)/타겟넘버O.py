22c0e7c10c0c2fdc4493a0a25a7d7c3ef155a680
eastcold <67042697+kdch9495@users.noreply.github.com>
2022-05-09 17:33:48 +0900

    Update 타겟넘버O.py

--- 프로그래머스/코딩테스트 연습/깊이,너비 우선 탐색(DFS,BFS)/타겟넘버O.py
+++ 프로그래머스/코딩테스트 연습/깊이,너비 우선 탐색(DFS,BFS)/타겟넘버O.py
@@ -2,15 +2,17 @@
 
 # bfs1
 def solution(numbers, target):
-    answer = 0
-    rs = [0]
-    for num in numbers:
-        tmp = []
-        for val in rs:
-            tmp.append(val+num)
-            tmp.append(val-num)
-        rs = tmp
-    return rs.count(target)
+    # -1, +1한 값을 자료구조에 저장/ 새로운 값은 pop해서 삭제 / len(numbers)만큼 돌렸을 때 stop 후, 값 카운트
+    from collections import deque
+    tmp = [0]
+    for nums in numbers:
+        tmp2 = []
+        for i in tmp:
+            tmp2.append(i+nums)
+            tmp2.append(i-nums)
+        tmp = tmp2
+        
+    return tmp2.count(target)
 
 
 # pythonic
