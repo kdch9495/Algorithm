4f35bb72c997cabf6f35831cf1f8a206782c747e
eastcold <67042697+kdch9495@users.noreply.github.com>
2022-05-10 17:26:52 +0900

    Update 가사 검색x.py

--- 프로그래머스/코딩테스트 연습/2020 KAKAO BLIND RECRUITMENT/가사 검색x.py
+++ 프로그래머스/코딩테스트 연습/2020 KAKAO BLIND RECRUITMENT/가사 검색x.py
@@ -1,6 +1,6 @@
 # https://programmers.co.kr/learn/courses/30/lessons/60060?language=python3
 
-# 시간 초과
+# 효율성 테스트 실패1
 def solution(words, queries):
     answer = []
     for query in queries:
@@ -17,3 +17,32 @@ def solution(words, queries):
                     cnt += 1
         answer.append(cnt)
     return answer
+
+# 효율성 테스트 실패1
+def solution(words, queries):
+    from collections import deque
+    answer = []
+    
+    for query in queries:
+        print(query)
+        cnt = 0
+        tmp_q = deque()
+        len_q = deque()
+        for i in range(len(query)):
+            if query[i] != "?":
+                tmp_q.append(i)
+            elif query[i] == "?":
+                len_q.append(i)
+
+        for word in words:
+            if tmp_q[0] == 0:
+                if len(query) == len(word):
+                    if query[:int(len_q[0])] == word[:int(len_q[0])]:
+                        cnt += 1
+            elif len_q[0] == 0:
+                if len(query) == len(word):
+                    if query[int(tmp_q[0]):] == word[int(tmp_q[0]):]:
+                        cnt += 1
+
+        answer.append(cnt)
+    return answer 
